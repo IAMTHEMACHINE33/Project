6becc9b201c49b9276f0d206deb63343a986b228
IAMTHEMACHINE33 <iamthemachine33@gmail.com>
2021-07-14 10:14:04 +0545

    all

---
+++ JavaScript/javascript.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    console.log("Button clicked!")
+    window.alert("Button click alert!")
+    // document.write("This is content from JavaScipt")
+    }
+document.getElementById("para1").innerHTML="Paragraph from javasript";
